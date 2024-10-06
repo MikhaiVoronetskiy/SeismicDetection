810a381f93a009b368a8b5e0b26fd5eb736ceeb1
MikhaiVoronetskiy <mikeva1801@gmail.com>
2024-10-06 13:21:54 +0100

    find_evidense fix

--- find_evidence.py
+++ find_evidence.py
@@ -12,11 +12,8 @@ def find_evidence(filename, time, range_detection=2000):
     for i in range(len(filenames)):
         if filenames[i] == filename:
             catalog_time = times[i]
-            if (catalog_time - time) < range_detection:
+            if abs(catalog_time - time) < range_detection:
                 return types[i]
     return "no evidence"
 
 
-csv_file_path1 = 'space_apps_2024_seismic_detection/data/lunar/training/data/S12_GradeA/xa.s12.00.mhz.1971-10-18HR00_evid00043.csv'
-a = find_evidence("xa.s12.00.mhz.1971-10-18HR00_evid00043",  10740)
-print(a)
