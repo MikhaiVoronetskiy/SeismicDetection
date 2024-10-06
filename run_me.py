239b2ae4f066a1dda1e7b9264daa5d13c72632dd
detojed <21GSovetkin@bromsgrove-school.co.uk>
2024-10-06 22:51:54 +0100

    Update run_me.py
    .

--- run_me.py
+++ run_me.py
@@ -1,17 +1,31 @@
 import pandas as pd
 import os
 import getXandY
-
+import csv
 
 def find_seismic_events(path_to_csv_file):
-    x = []
+    x = {}
+    type_dict = {
+        1: "shallow_mq",
+        2: "impact_mq",
+        3: "deep_mq",
+        0: "noise"
+
+    }
+    res_file = "result_catalog.csv"
+    with 
     filenames = os.listdir(os.getcwd())
     for i in filenames:
         if (i.split(".")[-1] =="csv") and i[:5] == "test"):
-            x.extend(getXandY.get_x_from_pickle(i))
+            x.update(getXandY.get_x(i))
     pipline = joblib.load('model.joblib') #insert model name
-    y_res = pipline.predict(x)
-    
+    y_res = pipline.predict(x.values)
+    for j in range(len(y_res)):
+        if y_res[j] == 0:
+            continue
+        typename = type_dict[y_res[j]] # name of type
+        time = list(x.keys())[j]
+        
     #if not noise print type and time
 
 
@@ -22,5 +36,5 @@ def teach_model_and_save():
     x, y = getXandY.getXandY()
     directory = 'space_apps_2024_seismic_detection/data/lunar/training/data/S12_GradeA' #please replace if directory is different
     getXandY.compile_x_y(directory=directory) #this can take a lot of time
-    #teach model
+    ai.main_ai(x, y)
     #save model
